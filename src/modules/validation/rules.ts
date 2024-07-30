7f5d55fa64dd5c2c839c600769474bf9148f8042
Cyril Auquier <=>
2024-07-30 19:47:48 +0200

    add validation rules

--- src/modules/validation/rules.ts
+++ src/modules/validation/rules.ts
@@ -1,20 +1,23 @@
 import * as yup from 'yup'
 
-export const email = yup.string().required().email()
-export const pseudo = yup.string().required()
+export const email = yup
+  .string()
+  .required('The email is required')
+  .email('You need to enter a valid email')
+export const pseudo = yup.string().required('The pseudo is required')
 export const password = yup
   .string()
-  .required()
+  .required('The password is required')
   .test('len', 'The password must have 8 caracters minimum', (value) => value.length >= 8)
 export const confirmationPassword = yup
   .string()
   .required()
   .oneOf([yup.ref('password')], 'Passwords must match')
-export const firstName = yup.string().required()
-export const lastName = yup.string().required()
+export const firstName = yup.string().required('The firstname is required')
+export const lastName = yup.string().required('The lastname is required')
 export const birthdate = yup
   .date()
-  .required()
+  .required('The birthdate is required')
   .test(
     'age',
     'User must be 13 years old to be on the plateform',
