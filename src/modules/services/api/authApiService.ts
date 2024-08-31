6df2fd30b755ee6366c82f8b61074f8900aefd45
Cyril Auquier <=>
2024-08-31 12:41:49 +0200

    US-2.2: - log user in and return it

--- src/modules/services/api/authApiService.ts
+++ src/modules/services/api/authApiService.ts
@@ -1,10 +1,10 @@
 import type { User } from '@/modules/domain/user'
 import type AuthService from '../authService'
 import ServiceError from '../error/serviceError'
-import type UserRegisterRepository from '@/modules/repository/userRepository'
 import userMapper from '@/modules/mappers/userMapper'
 import BaseError from '@/modules/repository/error/baseError'
 import type { RegisterDTO } from '@/modules/dto/registerDto'
+import type UserRegisterRepository from '@/modules/repository/userAuthRepository'
 
 export default class AuthApiService implements AuthService {
   constructor(private registerRepo: UserRegisterRepository) {}
@@ -17,6 +17,18 @@ export default class AuthApiService implements AuthService {
           birthDate: user.birthDate.replace('-', '/')
         })) !== undefined
       )
+    } catch (error) {
+      if (error instanceof BaseError) {
+        throw new ServiceError(error.message)
+      }
+      throw new ServiceError('An unexpected error occured')
+    }
+  }
+
+  async loginWithLogin(login: string, password: string): Promise<User | null> {
+    try {
+      const result = userMapper.toEntity(await this.registerRepo.loginWithPseudo(login, password))
+      return result
     } catch (error) {
       if (error instanceof BaseError) {
         throw new ServiceError(error.message)
--- src/modules/services/authService.ts
+++ src/modules/services/authService.ts
@@ -3,4 +3,5 @@ import type { RegisterDTO } from '../dto/registerDto'
 
 export default interface AuthService {
   registerUser(user: RegisterDTO): Promise<boolean>
+  loginWithLogin(login: string, password: string): Promise<User | null>
 }
