44f7a383a33f7885ca44ef8d9491541cff673d71
Cyril Auquier <=>
2024-09-10 22:12:38 +0200

    US-2.6: add a star icon

---
+++ src/components/cards/StarIcon.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <svg
+      width="35"
+      xmlns="http://www.w3.org/2000/svg"
+      height="33"
+      id="screenshot-47fd956f-cfd1-8074-8004-eea9237d22c7"
+      viewBox="972 156 35.063 33"
+      style="-webkit-print-color-adjust::exact"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      fill="none"
+      version="1.1"
+    >
+      <g id="shape-47fd956f-cfd1-8074-8004-eea9237d22c7" data-testid="Rectangle">
+        <defs></defs>
+        <g class="fills" id="fills-47fd956f-cfd1-8074-8004-eea9237d22c7">
+          <path
+            d="M972.000,167.000L983.966,167.000L989.253,156.000L994.540,166.436L1007.063,166.718L998.158,176.872L1001.219,189.000L990.088,184.205L977.565,188.436L981.183,176.026L972.000,167.000Z"
+            style="fill: #adff2f; fill-opacity: 1"
+          ></path>
+        </g>
+      </g>
+    </svg>
+    <span>5</span>
+  </div>
+</template>
+<script setup lang="ts"></script>
+<style lang="css" scoped>
+span {
+  position: absolute;
+}
+
+div {
+  width: 35px;
+  height: 33px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
