4eb9068af05570fe08b0a674d481522fb7952b0a
Cyril Auquier <=>
2024-09-11 12:19:07 +0200

    US-2.6: - display a search bar - display action buttons - style them

--- src/views/FeedView.vue
+++ src/views/FeedView.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="feed">
-    <QuizCard v-for="(item, index) in quizzes" :key="index" />
+    <div class="actions-wrapper">
+      <div class="filtering-wrapper">
+        <div>Sort</div>
+        <div>Filter</div>
+      </div>
+      <SearchBar />
+    </div>
+    <div class="wrapper">
+      <QuizCard v-for="(item, index) in quizzes" :key="index" class="quiz-card" />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import QuizCard from '../components/cards/QuizCard.vue'
+import SearchBar from '../components/form/SearchBar.vue'
 import { ref } from 'vue'
 
 const quizzes = ref([5, 1, 8, 3])
 </script>
-<style lang="css">
-.feed {
+<style lang="css" scoped>
+.feed,
+.feed .wrapper,
+.actions-wrapper,
+.filtering-wrapper {
   display: flex;
   flex-wrap: wrap;
+}
+
+.feed .wrapper {
+  justify-content: center;
   width: auto;
+}
+
+.feed .actions-wrapper {
+  margin-inline: 25px;
+  width: 100%;
+  justify-content: space-between;
+}
+
+.quiz-card {
+  margin-block: 10px;
 }
 </style>
