53f930c3ad7b3a3685ae3479b85cfca5a8f3d9e3
Cyril Auquier <=>
2024-09-11 20:36:12 +0200

    Add icons

---
+++ src/components/icons/IconFilter.vue
@@ -0,0 +1,51 @@
+<template>
+  <svg
+    width="25"
+    xmlns="http://www.w3.org/2000/svg"
+    height="25"
+    id="screenshot-2db0f6ce-e141-809d-8004-f0cafebe7f94"
+    viewBox="1713 235 25 25"
+    style="-webkit-print-color-adjust::exact"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    fill="none"
+    version="1.1"
+  >
+    <g
+      id="shape-2db0f6ce-e141-809d-8004-f0cafebe7f94"
+      data-testid="svg"
+      style="fill: #000000"
+      height="24"
+      width="24"
+      rx="0"
+      ry="0"
+    >
+      <g
+        id="shape-2db0f6ce-e141-809d-8004-f0cafebf7e7f"
+        data-testid="base-background"
+        style="display: none"
+      >
+        <g class="fills" id="fills-2db0f6ce-e141-809d-8004-f0cafebf7e7f">
+          <rect
+            rx="0"
+            ry="0"
+            x="1713"
+            y="235"
+            transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)"
+            width="25"
+            height="25"
+            fill="none"
+            style="fill: none"
+          ></rect>
+        </g>
+      </g>
+      <g id="shape-2db0f6ce-e141-809d-8004-f0cafebfc95b" data-testid="svg-path">
+        <g class="fills" id="fills-2db0f6ce-e141-809d-8004-f0cafebfc95b">
+          <path
+            d="M1723.417,253.750L1723.417,251.667L1727.583,251.667L1727.583,253.750L1723.417,253.750ZZM1719.250,248.542L1719.250,246.458L1731.750,246.458L1731.750,248.542L1719.250,248.542ZZM1716.125,243.333L1716.125,241.250L1734.875,241.250L1734.875,243.333L1716.125,243.333ZZ"
+            style="fill: #e8eaed"
+          ></path>
+        </g>
+      </g>
+    </g>
+  </svg>
+</template>
---
+++ src/components/icons/IconSort.vue
@@ -0,0 +1,51 @@
+<template>
+  <svg
+    width="25"
+    xmlns="http://www.w3.org/2000/svg"
+    height="25"
+    id="screenshot-2db0f6ce-e141-809d-8004-f0ca953e772a"
+    viewBox="1622 235 25 25"
+    style="-webkit-print-color-adjust::exact"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    fill="none"
+    version="1.1"
+  >
+    <g
+      id="shape-2db0f6ce-e141-809d-8004-f0ca953e772a"
+      data-testid="svg"
+      style="fill: #000000"
+      height="24"
+      width="24"
+      rx="0"
+      ry="0"
+    >
+      <g
+        id="shape-2db0f6ce-e141-809d-8004-f0ca953f1c7e"
+        data-testid="base-background"
+        style="display: none"
+      >
+        <g class="fills" id="fills-2db0f6ce-e141-809d-8004-f0ca953f1c7e">
+          <rect
+            rx="0"
+            ry="0"
+            x="1622"
+            y="235"
+            transform="matrix(1.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000)"
+            width="25"
+            height="25"
+            fill="none"
+            style="fill: none"
+          ></rect>
+        </g>
+      </g>
+      <g id="shape-2db0f6ce-e141-809d-8004-f0ca953f67fc" data-testid="svg-path">
+        <g class="fills" id="fills-2db0f6ce-e141-809d-8004-f0ca953f67fc">
+          <path
+            d="M1625.125,253.750L1625.125,251.667L1631.375,251.667L1631.375,253.750L1625.125,253.750ZZM1625.125,248.542L1625.125,246.458L1637.625,246.458L1637.625,248.542L1625.125,248.542ZZM1625.125,243.333L1625.125,241.250L1643.875,241.250L1643.875,243.333L1625.125,243.333ZZ"
+            style="fill: #e8eaed"
+          ></path>
+        </g>
+      </g>
+    </g>
+  </svg>
+</template>
