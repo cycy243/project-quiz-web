858dc23fb620f3a62ee61667e37e698471939840
Cyril Auquier <=>
2024-10-09 15:59:53 +0200

    - define emits used by every quesiton type - create component that will be the form of the single choice question

---
+++ src/components/quiz/questions/QuestionEmitsType.ts
@@ -0,0 +1,3 @@
+export type QuestionEmitsType = {
+  (e: 'submit', value: any): void
+}
---
+++ src/components/quiz/questions/SingleChoiceQuestion.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h4>Single choice question</h4>
+  </div>
+</template>
+<script setup lang="ts">
+import type { QuestionEmitsType } from './QuestionEmitsType'
+
+const emit = defineEmits<QuestionEmitsType>()
+</script>
+<style lang=""></style>
